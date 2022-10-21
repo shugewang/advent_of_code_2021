d22c1e9825a2a4a143ff6692d79c852636e1ecc7
Shu Wang <shu.wang@autotrader.co.uk>
2022-10-21 14:10:40 +0100

    Add day 2 part 1 file

---
+++ day2.py
@@ -0,0 +1,19 @@
+def calculate_position(input_file_name):
+    f = open(input_file_name, "r")
+    planned_course = list(f.read().splitlines())
+
+    horizontal = 0
+    depth = 0
+
+    for command in planned_course:
+        command_arr = command.split(" ")
+        if command_arr[0] == "forward":
+            horizontal += int(command_arr[1])
+        if command_arr[0] == "up":
+            depth -= int(command_arr[1])
+        if command_arr[0] == "down":
+            depth += int(command_arr[1])
+    print(horizontal*depth)
+
+
+calculate_position("plannedCourse.txt")
