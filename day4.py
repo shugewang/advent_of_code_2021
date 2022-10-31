e99e3d93d809986e4803bce4a1159e4cd2788547
Shu Wang <shu.wang@autotrader.co.uk>
2022-10-31 16:40:49 +0000

    Add day 4 refactored solution

--- day4.py
+++ day4.py
@@ -1,29 +1,9 @@
-import re
-
 def get_bingo(input_file_name):
     f = open(input_file_name, "r")
     bingo = list(f.read().splitlines())
     numbers_drawn = bingo.pop(0).split(',')
-    return numbers_drawn, bingo
-
-
-def convert_to_2d_array(bingo):
-    bingo_2d_array = [[]]
-    count = 0
-    board_number = 0
-    for x in bingo:
-        if x != '':
-            if count != 0 and count % 5 == 0:
-                bingo_2d_array.append([])
-                count = 0
-                board_number += 1
-                bingo_2d_array[board_number].append(x)
-            else:
-                bingo_2d_array[board_number].append(x)
-            count += 1
-            print(x)
-            print(count)
-            print(bingo_2d_array)
+    bingo_2d_array = convert_to_bingo_dict(bingo)
+    return numbers_drawn, bingo_2d_array
 
 
 def convert_to_bingo_dict(bingo):
@@ -32,62 +12,59 @@ def convert_to_bingo_dict(bingo):
     bingo_dict = {board_number: []}
     for x in bingo:
         if x != '':
+            x_arr = list(filter(lambda a: a != '', x.split(' ')))
             if count != 0 and count % 5 == 0:
                 count = 0
                 board_number += 1
-                bingo_dict[board_number] = [x]
+                bingo_dict[board_number] = [x_arr]
             else:
-                bingo_dict[board_number].append(x)
+                bingo_dict[board_number].append(x_arr)
             count += 1
     return bingo_dict
 
 
-def check_winner(numbers_drawn, bingo_dict):
+def check_winner(numbers_drawn, bingo):
     win = False
     for number in numbers_drawn:
-        for board_number, board in bingo_dict.items():
+        for board_number, board in bingo.items():
             for i in range(len(board)):
-                # print('row',board)
-                bingo_dict[board_number][i] = re.sub(r'\b%s\b' % number, "y", bingo_dict[board_number][i])
-                # print(bingo_dict[board_number])
-                if bingo_dict[board_number][i].count('y') == 5:
-                    print("there's a win", number)
-                    print(bingo_dict[board_number])
-                    print(board_number)
+                bingo[board_number][i] = [check_if_called(x, number) for x in board[i]]
+                if board[i][0] == board[i][1] == board[i][2] == board[i][3] == board[i][4] or board[0][i] == board[1][i] == board[2][i] == board[3][i] == board[4][i]:
+                    print("winning board number",board_number)
                     win = True
                     break
-            win = check_columns(board_number, board, win)
             if win:
-                get_winning_score(bingo_dict, board_number, number)
-                print(board_number)
-                print(bingo_dict[board_number])
                 break
+        if win:
+            winning_score = get_winning_score(bingo, board_number, number)
+            return winning_score
 
 
-def check_columns(board_number, board, win):
-    col_board = [elem for elem in board]
-    for i in range(len(col_board)):
-        col_board[i] = list(filter(lambda a: a != '', col_board[i].split(' ')))
-        if col_board[i][0] == col_board[i][1] == col_board[i][2] == col_board[i][3] == col_board[i][4]:
-            win = True
-            print('col')
-    return win
+def check_if_called(bingo_number, number):
+    if bingo_number == number:
+        return "y"
+    else:
+        return bingo_number
 
 
+def get_winning_score(bingo, board_number, number):
+    unmarked_sum = 0
+    for i in bingo[board_number]:
+         for j in i:
+             if j != 'y':
+                 unmarked_sum += int(j)
+    return unmarked_sum*int(number)
 
-def get_winning_score(bingo_dict, board_number, number):
-    winning_board = " ".join(bingo_dict[board_number])
-    winning_board = winning_board.replace("y", "").split(" ")
-    unmarked_sum = sum(list(map(int, list(filter(lambda a: a != '', winning_board)))))
-    print(unmarked_sum*int(number))
 
+# def get_last_board():
+#     count = len(bingo)
 
 def main():
     numbers_drawn, bingo = get_bingo("bingo.txt")
-    bingo_dict = convert_to_bingo_dict(bingo)
-    print("numbers", numbers_drawn)
-    print(bingo_dict)
-    check_winner(numbers_drawn, bingo_dict)
+    print(numbers_drawn)
+    print(bingo)
+    winning_score = check_winner(numbers_drawn, bingo)
+    print(winning_score)
 
 
 main()
---
+++ day4old.py
@@ -0,0 +1,94 @@
+import re
+
+
+def get_bingo(input_file_name):
+    f = open(input_file_name, "r")
+    bingo = list(f.read().splitlines())
+    numbers_drawn = bingo.pop(0).split(',')
+    return numbers_drawn, bingo
+
+
+# def convert_to_2d_array(bingo):
+#     bingo_2d_array = [[]]
+#     count = 0
+#     board_number = 0
+#     for x in bingo:
+#         if x != '':
+#             if count != 0 and count % 5 == 0:
+#                 bingo_2d_array.append([])
+#                 count = 0
+#                 board_number += 1
+#                 bingo_2d_array[board_number].append(x)
+#             else:
+#                 bingo_2d_array[board_number].append(x)
+#             count += 1
+#             print(x)
+#             print(count)
+#             print(bingo_2d_array)
+
+
+def convert_to_bingo_dict(bingo):
+    count = 0
+    board_number = 1
+    bingo_dict = {board_number: []}
+    for x in bingo:
+        if x != '':
+            if count != 0 and count % 5 == 0:
+                count = 0
+                board_number += 1
+                bingo_dict[board_number] = [x]
+            else:
+                bingo_dict[board_number].append(x)
+            count += 1
+    return bingo_dict
+
+
+def check_winner(numbers_drawn, bingo_dict):
+    win = False
+    for number in numbers_drawn:
+        for board_number, board in bingo_dict.items():
+            for i in range(len(board)):
+                # print('row',board)
+                bingo_dict[board_number][i] = re.sub(r'\b%s\b' % number, "y", bingo_dict[board_number][i])
+                # print(bingo_dict[board_number])
+                if bingo_dict[board_number][i].count('y') == 5:
+                    print("there's a win", number)
+                    print(bingo_dict[board_number])
+                    print(board_number)
+                    win = True
+                    break
+            win = check_columns(board_number, board, win)
+            if win:
+                get_winning_score(bingo_dict, board_number, number)
+                print(board_number)
+                print(bingo_dict[board_number])
+                break
+
+
+def check_columns(board_number, board, win):
+    col_board = [elem for elem in board]
+    for i in range(len(col_board)):
+        col_board[i] = list(filter(lambda a: a != '', col_board[i].split(' ')))
+        if col_board[i][0] == col_board[i][1] == col_board[i][2] == col_board[i][3] == col_board[i][4]:
+            win = True
+            print('col')
+    return win
+
+
+
+def get_winning_score(bingo_dict, board_number, number):
+    winning_board = " ".join(bingo_dict[board_number])
+    winning_board = winning_board.replace("y", "").split(" ")
+    unmarked_sum = sum(list(map(int, list(filter(lambda a: a != '', winning_board)))))
+    print(unmarked_sum*int(number))
+
+
+def main():
+    numbers_drawn, bingo = get_bingo("bingo.txt")
+    bingo_dict = convert_to_bingo_dict(bingo)
+    print("numbers", numbers_drawn)
+    print(bingo_dict)
+    check_winner(numbers_drawn, bingo_dict)
+
+
+main()
